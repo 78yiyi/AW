9bf762b3dbc14ec3e85e80643126646804edee05
78yiyi <lparra@lasallegrinon.es>
2024-01-18 18:07:48 +0100

    First Commit

---
+++ JS/videojs01.js
@@ -0,0 +1,21 @@
+/* for (let i = 10; i > 0; i--) {
+    alert(i);    
+} */
+
+/* let i=10;
+do {    
+    alert("Estoy en el bucle");
+    
+} while (i>=10); */
+/* document.getElementById('Boton').addEventListener('click', function() {
+    mostrarDatos();
+}); */
+
+function Pulsar() {
+
+    var name=document.getElementById("nombre").value;
+    var msg="Tu nombre es " +name;
+    document.getElementById("JS-Container").textContent = msg;
+}
+
+
