4cfff792a7720fdf5046393343f5c329b2bb0711
78yiyi <lparra@lasallegrinon.es>
2024-11-10 18:22:22 +0100

    js01

--- JS/JSclase01.js
+++ JS/JSclase01.js
@@ -1,3 +1,14 @@
 let a=2;
+let cliente={
+    User: "lparra",
+    FullName:"Leonardo Parra",
+    email:"lparra@lasallegrinon.es",
+    edad: 46,
+}
+
 
 console.log("El valor de a es "+a+" y su tipo es "+ typeof a);
+console.log(cliente);
+console.log(cliente.edad);
+
+cliente.edad=17;
