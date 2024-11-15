a895fe5c1ea2482852b0333f5d63f94be387fc59
leo <juanleonardo78@gmail.com>
2024-11-15 10:44:44 +0100

    clase viernes

--- JS/formjs.js
+++ JS/formjs.js
@@ -2,4 +2,5 @@ function ShowName(){
     /*alert("Entró");*/
     let name=document.getElementById("NameField").value;
     document.getElementById("Result").textContent=name;
+    document.getElementById("Result").innerHTML=`<H1>Hola<H1>`;
 }
