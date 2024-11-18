7756fe64f56f5a2c4ad0ff74c6d4d8922e7184de
78yiyi <lparra@lasallegrinon.es>
2024-11-18 19:31:17 +0100

    asd

--- JS/formjs.js
+++ JS/formjs.js
@@ -1,5 +1,22 @@
+const btn=document.querySelector("#btnSubmit");
+btn.addEventListener('click',() => {
+    //let name=document.getElementById("NameField").value;
+    const name=document.querySelector("#NameField");
+    document.getElementById("Result").innerHTML="<H1>"+name.value+"</H1>";    
+})
+
 function ShowName(){
     /*alert("Entró");*/
-    let name=document.getElementById("NameField").value;
-    document.getElementById("Result").textContent=name;
+    const name=document.getElementById("NameField").value;
+    document.getElementById("Result").innerHTML="<H1>"+name.value+"</H1>";
+    //document.getElementById("Result").textContent=name;
+    let c=confirm("Deseas seguir");
+    if (!c) {
+        alert("Has pulsado finalizar");
+    } else {
+        alert("Has pulsado seguir");
+    }
+    while (c) {
+        c=confirm("Deseas seguir");
+    }    
 }
